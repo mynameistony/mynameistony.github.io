d0623c3bbbb4f73e0f11467db0147632687bbcf8
Tony <tonyrogers10195@gmail.com>
2015-02-24 23:52:10 -0800

    Test

--- quick-push.sh
+++ quick-push.sh
@@ -8,11 +8,11 @@ else
 	echo "Quickly pushing website..."
 
 	echo -n "Adding files..."
-	git add *
+	git add * 2> /dev/null
 	echo "Done"
 
 	echo -n "Commiting Changes..."
-	git commit -m "$@"
+	git commit -m "$@" 2> /dev/null
 	echo "Done"
 	echo "Commit Message: $@"
 
