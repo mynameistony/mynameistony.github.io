58e05d72f6ebbab198ba3419995a6402aa97dd48
Tony <tonyrogers10195@gmail.com>
2015-02-27 08:53:15 -0800

    Unit converter up

--- scripts.js
+++ scripts.js
@@ -1,57 +1,378 @@
-function setUnitType(){
-	var thisType = document.getElementById("current-unit-type").value;
+var lengthUnits = ["Inches", "Feet", "Yards", "Miles","Meters"];
+var volumeUnits = ["Fl. Oz", "Cups", "Pints", "Quarts", "Gallons", "Liters"]; //Liter is French for "Give me some FUCKING cola"!
+var tempUnits = ["Farenheit", "Celsius", "Kelvin"];
 
-	switch(thisType){
+function actuallyConvert(unitType, startUnit, endUnit, amount){
+	var result = "Haven't converted yet";
+	switch(unitType){
+		case "Length":
+			switch(startUnit){
 
-		case "Volume":
-			selectStartUnit = document.getElementById('start-unit');
-			while (selectStartUnit.hasChildNodes()) {  
-    				selectStartUnit.removeChild(selectStartUnit.firstChild);
+				case "Inches":
+					switch(endUnit){
+
+						case "Feet":
+							result = amount / 12;
+						break;
+						case "Yards":
+							result = amount / 36;
+						break;
+						case "Miles":
+							result = (amount / 12) / 5280;
+						break;						
+						case "Meters":
+							result = amount * .0254;
+						break;
+
+					}
+				break;
+				case "Feet":
+					switch(endUnit){
+						case "Inches":
+							result = amount * 12;
+						break;
+						case "Yards":
+							result = amount / 3;
+						break;
+						case "Miles":
+							result = amount / 5280;
+						break;						
+						case "Meters":
+							result = amount *.3048;
+						break;
+					}
+				break;
+				case "Yards":
+					switch(endUnit){				
+						case "Inches":
+							result = amount * 36;
+						break;
+						case "Feet":
+							result = amount * 3;
+						break;
+						case "Miles":
+							result = amount / 51;
+						break;						
+						case "Meters":
+							result = amount * .9144;
+						break;
+					}
+				break;
+				case "Miles":
+					switch(endUnit){
+						case "Inches":
+							result = amount * 5280 * 12;
+						break;
+						case "Yards":
+							result = amount * 51;
+						break;
+						case "Feet":
+							result = amount * 5280;
+						break;						
+						case "Meters":
+							result = amount * 1609.34;
+						break;
+					}
+				break;
+				case "Meters":
+					switch(endUnit){
+						case "Inches":
+							result = amount * 39.3701;
+						break;
+						case "Yards":
+							result = amount * 1.09361;
+						break;
+						case "Feet":
+							result = amount * 3.28084;
+						break;						
+						case "Miles":
+							result = amount * 0.000621371;
+						break;
+					}
+				break;
+
 			}
 		break;
 
+		case "Volume":
+
+			switch(startUnit){
+				
+				case "Fl. Oz":
+					switch(endUnit){
+						case "Cups":
+							result = amount / 8;
+						break;
+						case "Pints":
+							result = amount / 16;
+						break;
+						case "Quarts":
+							result = amount / 32;
+						break;
+						case "Gallons":
+							result = amount / 128;
+						break;
+						case "Liters":
+							result = amount * 0.0295735;
+						break;
+					}
+				break;
+				case "Cups":
+					switch(endUnit){
+						case "Fl. Oz":
+							result = amount * 8;
+						break;
+						case "Pints":
+							result = amount / 2;
+						break;
+						case "Quarts":
+							result = amount / 4;
+						break;
+						case "Gallons":
+							result = amount / 16;							
+						break;
+						case "Liters":
+							result = amount * 0.236588;
+						break;
+					}
+				break;
+				case "Pints":
+					switch(endUnit){
+						case "Cups":
+							result = amount * 2;
+						break;
+						case "Fl. Oz":
+							result = amount * 16;
+						break;
+						case "Quarts":
+							result = amount / 2;
+						break;
+						case "Gallons":
+							result = amount / 8;
+						break;
+						case "Liters":
+							result = amount * 0.473176;
+						break;
+					}
+				break;
+				case "Quarts":
+					switch(endUnit){
+						case "Cups":
+							result = amount * 4;							
+						break;
+						case "Pints":
+							result = amount * 2;
+						break;
+						case "Fl. Oz":
+							result = amount * 32;
+						break;
+						case "Gallons":
+							result = amount / 4;
+						break;
+						case "Liters":
+							result = amount * 0.946353;
+						break;
+					}
+				break;
+				case "Gallons":
+					switch(endUnit){
+						case "Cups":
+							result = amount * 16;							
+						break;
+						case "Pints":
+							result = amount * 8;
+						break;
+						case "Quarts":
+							result = amount * 4;
+						break;
+						case "Fl. Oz":
+							result = amount * 128;
+						break;
+						case "Liters":
+							result = amount * 3.78541;
+						break;
+					}
+				break;
+				case "Liters":
+					switch(endUnit){
+						case "Cups":
+							result = amount * 4.22675;
+						break;
+						case "Pints":
+							result = amount * 2.11338;
+						break;
+						case "Quarts":
+							result = amount * 1.05669;
+						break;
+						case "Gallons":
+							result = amount * 0.264172;
+						break;
+						case "Fl. Oz":
+							result = amount * 33.814;
+						break;
+					}
+				break;
+
+			}
+		break;
+
+		case "Temperature":
+
+			switch(startUnit){
+
+				case "Farenheit":
+					switch(endUnit){
+						case "Kelvin":
+						break;
+						case "Celsius":
+						break;
+					}
+				break;
+				case "Celsius":
+					switch(endUnit){
+						case "Kelvin":
+						break;
+						case "Farenheit":
+						break;
+					}
+				break;
+				case "Kelvin":
+					switch(endUnit){
+						case "Farenheit":
+						break;
+						case "Celsius":
+						break;
+					}
+				break;
+			}
+		break;
+
+		default:
+		break;
+
+
+
+	}
+
+	return result;
+
+}
+
+function convert(){
+
+	var currentType = document.getElementById("current-unit-type").value; // Get current type of unit
+
+	var startUnit = document.getElementById("start-unit").value; // Get starting unit
+
+	var endUnit = document.getElementById("end-unit").value; // Get ending unit
+
+	var startAmount = document.getElementById("start-amount").value; // Get amount
+
+	document.getElementById('converted-value').innerHTML = actuallyConvert(currentType,startUnit,endUnit,startAmount); //Convert and set element
+}
+
+function setStartUnit(){
+	var thisType = document.getElementById("current-unit-type").value; // Get current type of unit
+
+	var startingUnit = document.getElementById('start-unit').value; //Selector for starting unit
+
+	var endUnitSelector = document.getElementById('end-unit'); //Selector for starting unit
+
+	while (endUnitSelector.hasChildNodes()) {  
+		endUnitSelector.removeChild(endUnitSelector.firstChild);
+	}
+
+	switch(thisType){
 		case "Length":
-			selectStartUnit = document.getElementById('start-unit');
-			selectEndUnit = document.getElementById('end-unit');
-
-			while (selectStartUnit.hasChildNodes()) {  
-    				selectStartUnit.removeChild(selectStartUnit.firstChild);
-			}
-
-			while (selectEndUnit.hasChildNodes()) {  
-    				selectEndUnit.removeChild(selectEndUnit.firstChild);
-			}
-
-			var lengthUnits = ["Inches", "Feet", "Yards", "Miles","Meters"];
 			for (var i = 0; i<lengthUnits.length; i++){
 				var opt = document.createElement('option');
 				opt.value = lengthUnits[i];
 				opt.innerHTML = lengthUnits[i];
-				selectStartUnit.appendChild(opt);
+				if(lengthUnits[i] != startingUnit)
+					endUnitSelector.appendChild(opt);
 			}
-   
+		break;
+
+		case "Volume":
+			for (var i = 0; i<volumeUnits.length; i++){
+				var opt = document.createElement('option');
+				opt.value = volumeUnits[i];
+				opt.innerHTML = volumeUnits[i];
+				if(volumeUnits[i] != startingUnit)
+					endUnitSelector.appendChild(opt);
+			}
 		break;
 
 		case "Temperature":
-			selectStartUnit = document.getElementById('start-unit');
-			while (selectStartUnit.hasChildNodes()) {  
-    				selectStartUnit.removeChild(selectStartUnit.firstChild);
+			for (var i = 0; i<tempUnits.length; i++){
+				var opt = document.createElement('option');	
+				opt.value = tempUnits[i];
+				opt.innerHTML = tempUnits[i];
+				if(tempUnits[i] != startingUnit)
+					endUnitSelector.appendChild(opt);
 			}
+		break;
+	}
+
+	convert();
+}
+function setUnitType(){
+	var thisType = document.getElementById("current-unit-type").value; // Get current type of unit
+
+	var startUnitSelector = document.getElementById('start-unit'); //Selector for starting unit	
+
+	var endUnitSelector = document.getElementById('end-unit'); //Selector for starting unit
+
+	//Clear starting unit selector
+	while (startUnitSelector.hasChildNodes()) {  
+		startUnitSelector.removeChild(startUnitSelector.firstChild);
+	}
+
+	while (endUnitSelector.hasChildNodes()) {  
+		endUnitSelector.removeChild(endUnitSelector.firstChild);
+	}
+
+	switch(thisType){
+
+		case "Volume":
+			for (var i = 0; i<volumeUnits.length; i++){
+				var opt = document.createElement('option');
+				opt.value = volumeUnits[i];
+				opt.innerHTML = volumeUnits[i];
+				startUnitSelector.appendChild(opt);
+
+			}			
+		break;
+
+		case "Length":
+
+			for (var i = 0; i<lengthUnits.length; i++){
+				var opt = document.createElement('option');
+				opt.value = lengthUnits[i];
+				opt.innerHTML = lengthUnits[i];
+				startUnitSelector.appendChild(opt);
+
+			}
+		break;
+
+		case "Temperature":
+			for (var i = 0; i<tempUnits.length; i++){
+				var opt = document.createElement('option');
+				opt.value = tempUnits[i];
+				opt.innerHTML = tempUnits[i];
+				startUnitSelector.appendChild(opt);
+			}
+			
 
 		break;
 
 		default:
-			alert("Pick a unit dumbass!");
-
-			selectStartUnit = document.getElementById('start-unit');
-			while (selectStartUnit.hasChildNodes()) {  
-    				selectStartUnit.removeChild(selectStartUnit.firstChild);
-			}
-
+			alert("Pick a unit!");
 		break;
 
 	}
-//	alert(thisType);
+
+	setStartUnit();
 }
 
 function sayGreeting(){
